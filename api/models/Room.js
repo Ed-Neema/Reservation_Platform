f38fe2ebb83b71d35c15dd27e78001b80250941c
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-07-27 01:33:16 +0300

    rooms model

---
+++ api/models/Room.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const  {Schema, model} = mongoose;
+const RoomSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    roomNumbers: [{ number: Number, unavailableDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
+
+
+module.exports = model("Room", RoomSchema);
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { verifyAdmin } = require("../utils/verifyToken");
+const { createRoom, updateRoomAvailability, updateRoom, deleteRoom, getRoom, getRooms } = require("../controllers/room");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This is an auth endpoint");
-});
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
 
 module.exports = router;
